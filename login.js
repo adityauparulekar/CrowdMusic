a51635ab8c0c1bc0015893aacd4ccd4da0e6b905
Aditya Parulekar <adityauparulekar@gmail.com>
2019-11-02 16:27:10 -0500

    client for now

--- login.js
+++ login.js
@@ -1,10 +1,14 @@
 
+const create_url = "localhost:1232/create_room";
 function create() {
-   var socket = io();
-   console.log(document.getElementById("create_username").value);
-   socket.emit("create_room", {username: document.getElementById("create_username").value});
+   const username = document.getElementById("create_username").value;
+   const http = new XMLHttpRequest();
+   http.open("GET", create_url ); // false for synchronous request
+   http.send(null);
+   console.log(http.responseText);
 }
 
 function join() {
+
 
 }
