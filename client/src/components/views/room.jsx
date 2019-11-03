2fec76ca03160fad4496647f15dd168e10db4948
Aditya Parulekar <adityauparulekar@gmail.com>
2019-11-02 23:29:22 -0500

    fixedd bug

--- client/src/components/views/room.jsx
+++ client/src/components/views/room.jsx
@@ -40,31 +40,31 @@ export default class Room extends Component {
       <title>
     Room
   </title>
-  <div class="header">
+  <div className="header">
     ROOM
   </div>
   <link href="../../stylesheets/room.scss" rel="stylesheet" type="text/css"/>
   {/* <script type="text/javascript" src="room_user.js"></script> */}
-  <div class="room_user-page">
+  <div className="room_user-page">
 
-    <div class="song cs">
-      <div class="song_text">
+    <div className="song cs">
+      <div className="song_text">
         CURRENT SONG
       </div>
     </div>
 
 
-    <div class="song q">
-      <div class="song_text">
+    <div className="song q">
+      <div className="song_text">
         SONG QUEUE
       </div>
     </div>
     
-    <div class="song add">
-      <div class="song_text">
+    <div className="song add">
+      <div className="song_text">
         ADD SONG
       </div>
-      <form class="add_song-form">
+      <form className="add_song-form">
         <input type="song_text" placeholder="Song Name" id="add_song" />
         <button onClick="____()">Add To Queue</button>
       </form>
